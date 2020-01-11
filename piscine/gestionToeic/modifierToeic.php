3025ee2c3f1718a027cf0c36c3021ae6dbd8b346
lilianmisser <55981134+lilianmisser@users.noreply.github.com>
2020-01-11 17:32:58 +0100

    Add files via upload
    Modification toeic

--- piscine/gestionToeic/modifierToeic.php
+++ piscine/gestionToeic/modifierToeic.php
@@ -1,3 +1,31 @@
-<div class=container>
+<?php
+	include("connectbdd.php");
 
-</div>
+	if($requete = $bdd->prepare("SELECT nom_sujet FROM sujet_toeic ")){
+		$requete->execute();
+		$tab = get_result($requete);
+	}
+	$bdd->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Modifier Toeic</title>
+</head>
+<body>
+	<form method="post" action= "traitement/traitement_modifToeic.php">
+		<select name="nom_sujet">
+		<?php
+			for($i=0;$i<count($tab);$i++){
+		?>
+			<option value="<?php echo($tab[$i]["nom_sujet"])?>"> <?php echo($tab[$i]["nom_sujet"]) ?> </option>
+		<?php
+			}
+		?>
+		</select>
+		<br>
+		<input type="submit" value="Valider">
+	</form>
+</body>
+</html>
