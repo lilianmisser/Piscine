49f39dcc410f3f7a928779051facbe747f6f478c
AxelCanton <58460876+AxelCanton@users.noreply.github.com>
2020-01-16 20:33:28 +0100

    Add files via upload

--- traitement/traitement_modifToeic.php
+++ traitement/traitement_modifToeic.php
@@ -1,16 +1,22 @@
 <?php
 	include("../connectbdd.php");
+
+	//Fonction get_result($requete)
 	include("getResult.php");
 
+	//Si pas passé par la page modification toeic
 	if(!isset($_POST["id_sujet"])){
 		header("Location: ../accueil.php");
 		exit;
 	}
 
-	if($requete = $bdd->prepare("SELECT id_sujet,nom_sujet FROM sujet_toeic WHERE sujet_toeic.id_sujet = ?")){
+	//On récupère le sujet dans le BDD
+	if($requete = $bdd->prepare("SELECT id_sujet FROM sujet_toeic WHERE sujet_toeic.id_sujet = ?")){
 		$requete->bind_param("s",$_POST["id_sujet"]);
 		$requete->execute();
 		$id_sujet=get_result($requete);
+
+		//On récupère les questions associées au sujet
 		if($requete = $bdd->prepare("SELECT reponse FROM question WHERE question.id_sujet = ?")){
 			$requete->bind_param("i",$id_sujet[0]["id_sujet"]);
 			$requete->execute();
@@ -21,28 +27,19 @@
 ?>
 
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Modifier TOEIC</title>
-</head>
-<body>
+
 	<?php
-     echo '
-                    <form method="post" action="../gererToeic.php#e" id="form" name="form">';
+
+	//Envoi des questions par formulaire automatiquement
+     echo '<form method="post" action="../gererToeic.php#e" id="form" name="form">';
                     for ($i=1;$i<=200;$i++){
                 echo '<input type="hidden" name="quest',$i,'" value="',$tab[$i-1]["reponse"],'">';
              }
              echo '<input type="hidden" name="id_sujet" value="',$id_sujet[0]["id_sujet"],'">';
              echo '</form>';
                   
-                    echo '<script type="text/JavaScript">
-                    document.getElementById("form").submit();
-                    </script>';
-    	
-	?>
-	<input type="hidden" name="id_sujet" value="<?php echo($id_sujet["id_sujet"]); ?>" />
-	<input type = "submit" value = "Valider" />
- 	</form>
-</body>
-</html>
+			echo '<script type="text/JavaScript">
+					document.getElementById("form").submit();
+				</script>';
+			  
+ 	?>
